7a720f919a980761f25f6193896dc6ab51f4a758
aldia_mechdream <futami16237@gmail.com>
2026-05-17 23:34:48 +0800

    feat: 添加solarlunar依赖并扩展解析功能以支持农历信息

--- packages/divination-liuyao/src/parser.skill.ts
+++ packages/divination-liuyao/src/parser.skill.ts
@@ -1,5 +1,7 @@
+import solar2lunar from 'solarlunar';
 import { Hexagram } from './classes/hexagram.js';
 import { Yao } from './classes/yao.js';
+import { SixGodTable } from './core/six-gods.js';
 
 function getInfo() {
   const rawYaos = process.argv[2];
@@ -17,4 +19,40 @@ function getInfo() {
   return Hexagram.fromQuaternary(rawYaos) || Hexagram.fromId(rawYaos) || Hexagram.fromSymbolName(rawYaos);
 }
 
-console.log(getInfo()?.toAIReadable() || '无法识别输入的卦象');
+function getShichen(date: Date): string {
+  return '子丑丑寅寅卯卯辰辰巳巳午午未未申申酉酉戌戌亥亥子'[date.getHours()] ?? '未知';
+}
+
+function getLunarInfo() {
+  const rawDatetime = process.argv[3];
+  const datetime = new Date(rawDatetime);
+  if (Number.isNaN(datetime.getTime())) {
+    return null;
+  }
+
+  const l = solar2lunar(datetime.getFullYear(), datetime.getMonth() + 1, datetime.getDate());
+
+  if (l === -1) {
+    return null;
+  }
+
+  const shichen = getShichen(datetime);
+  const gods = SixGodTable.find((g) => g.heavenlyStem === shichen)!.gods;
+
+  return {
+    年: l.gzYear,
+    月: l.gzMonth,
+    日: l.gzDay,
+    时: getShichen(datetime),
+    六神顺序: {
+      上爻: gods[5],
+      五爻: gods[4],
+      四爻: gods[3],
+      三爻: gods[2],
+      二爻: gods[1],
+      初爻: gods[0],
+    },
+  };
+}
+
+console.log({ 卦象: getInfo()?.toAIReadable(), 时辰信息: getLunarInfo() });
