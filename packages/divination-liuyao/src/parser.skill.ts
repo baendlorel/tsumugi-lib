edb6617ad3b37ef0835a559d44b5944c21538be7
aldia_mechdream <futami16237@gmail.com>
2026-08-21 16:47:16 +0800

    feat: 现在改为输出真正的json，而不是js object literal

--- packages/divination-liuyao/src/parser.skill.ts
+++ packages/divination-liuyao/src/parser.skill.ts
@@ -53,4 +53,4 @@ function getLunarInfo() {
   };
 }
 
-console.dir({ version: '__VERSION__', 卦象: getInfo()?.toAIReadable(), 时辰: getLunarInfo() }, { depth: 6 });
+console.log(JSON.stringify({ version: '__VERSION__', 卦象: getInfo()?.toAIReadable(), 时辰: getLunarInfo() }, null, 2));
